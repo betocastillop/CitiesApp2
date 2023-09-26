115625680126c23c8dedb2c677e8af762951a50c
Alberto Castillo <betocastillop@gmail.com>
2023-09-25 21:04:30 -0600

    Comented Render function

--- App.js
+++ App.js
@@ -30,6 +30,7 @@ export default class App extends Component {
         this.setState({ cities });
     }
 
+    // Render - this function renders the screen
     render() {
         return (
             <Tabs screenProps={{
